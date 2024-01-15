b38c0197e1ec529ffdc94967df4e65e5da14cabf
Jhonatas <104805041+OxeLeming@users.noreply.github.com>
2024-01-15 16:20:10 -0300

    Restrição de quantidade zerada

--- js/app.js
+++ js/app.js
@@ -2,8 +2,11 @@
 let totalCarrinho = 0;
 limpar();
 
+
 //Selecionar o produto
-function adicionar(){
+function adicionar() {
+  let quantia = document.getElementById('quantidade').value;
+  if (quantia != 0) {
     var selecionado = document.getElementById("produto").value;
     var produto = selecionado.split('-')[0];
     var precoUnitario = selecionado.split('R$')[1];
@@ -12,19 +15,23 @@ function adicionar(){
     //calcular subtotal dos produtos selecionados
     var subtotalProduto = quantidade * precoUnitario;
 
-   //adicionar itens ao carrinho
-   var carrinho = document.getElementById('lista-produtos');
-   carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
-   <span class="texto-azul">${quantidade}x</span> ${produto} <span class="texto-azul">R$${subtotalProduto}</span>
- </section>`
+    //adicionar itens ao carrinho
+    var carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${produto} <span class="texto-azul">R$${subtotalProduto}</span>
+    </section>`
 
-  totalCarrinho = totalCarrinho + subtotalProduto;
-  document.getElementById('valor-total').textContent = `R$ ${totalCarrinho}`;
-  document.getElementById('quantidade').value = 0;
+    totalCarrinho = totalCarrinho + subtotalProduto;
+    document.getElementById('valor-total').textContent = `R$ ${totalCarrinho}`;
+    document.getElementById('quantidade').value = 0;
+
+  } else {
+    alert('Por favor, escolha a quantidade.');
+  }
 }
 
-function limpar(){
-document.getElementById('valor-total').textContent = "R$ 0";
-document.getElementById('quantidade').value = 0;
-document.getElementById('lista-produtos').innerHTML = '';
+function limpar() {
+  document.getElementById('valor-total').textContent = "R$ 0";
+  document.getElementById('quantidade').value = 0;
+  document.getElementById('lista-produtos').innerHTML = '';
 }
